9ccddf0862b6d41f31d47f5c807a7764dd98c33b
gilson-delgado <gcdelgaa@gmail.com>
2024-07-09 01:29:13 -0300

    sem alteraçoes

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -3,7 +3,8 @@ import "./button.css"
 function MeuButton() {
   const Clique = () => {
 
-    const label = "Baixar CV !"; // Substitua pela sua label
+    const label = "Baixar CV !";
+    // Substitua pela sua label
     alert(`A label desse botão é ${label}`);
   };
 
