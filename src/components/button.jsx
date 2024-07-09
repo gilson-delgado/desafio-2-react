60e12b2fa095d149f32ca39cef2465b7386ce307
gilson-delgado <gcdelgaa@gmail.com>
2024-07-09 00:50:42 -0300

    alterado cor do body para cadetblue e color do .btn:hover para gray no @media

---
+++ src/components/button.jsx
@@ -0,0 +1,20 @@
+import react, {Component} from 'react'; 
+import "./button.css"
+function MeuButton() {
+  const Clique = () => {
+
+    const label = "Baixar CV !"; // Substitua pela sua label
+    alert(`A label desse botão é ${label}`);
+  };
+
+  return (
+    <>  
+        <button className="btn" onClick={Clique}>Clique aqui</button>
+      
+    </>
+  );
+}
+
+export default MeuButton;
+
+
